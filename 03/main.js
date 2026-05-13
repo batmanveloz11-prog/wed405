51b51a868e5eb738db79ec04ca7dc542f6c7af2d
Gabriel Aguirre <Gabriel@hola.com>
2026-05-12 20:45:09 -0600

    Actualizar

--- 03/main.js
+++ 03/main.js
@@ -1,34 +1,57 @@
-var alto=document.getElementById("alto");
-var ancho=document.getElementById("ancho");
-var color=document.getElementById("color");
-var figura=document.getElementById("figura");
-var radius=document.getElementById("radius");
-var anchoborde=document.getElementById("anchoborde");
-var tipoborde=document.getElementById("tipoborde");
-var colorborde=document.getElementById("colorborde");
+var alto = document.getElementById("alto");
+var ancho = document.getElementById("ancho");
+var color = document.getElementById("color");
+var figura = document.getElementById("figura");
+var radius = document.getElementById("radius");
+var anchoborde = document.getElementById("anchoborde");
+var tipoborde = document.getElementById("tipoborde");
+var colorborde = document.getElementById("colorborde");
+var colorsombra = document.getElementById("colorsombra");
+var ejeX = document.getElementById("ejeX");
+var ejeY = document.getElementById("ejeY");
+var blur = document.getElementById("blur");
+var spread = document.getElementById("spread");
 
-alto.oninput=()=>{
-    figura.style.height=alto.value+"px";
-    document.getElementById("valto").innerHTML=alto.value;
+alto.oninput = () => {
+    figura.style.height = alto.value + "px";
+    document.getElementById("valto").innerHTML = alto.value;
 }
-ancho.oninput=()=>{
-    figura.style.width=ancho.value+"px";
-    document.getElementById("vancho").innerHTML=ancho.value;
+ancho.oninput = () => {
+    figura.style.width = ancho.value + "px";
+    document.getElementById("vancho").innerHTML = ancho.value;
 }
-color.onchange=()=>{
-    figura.style.backgroundColor=color.value;
+color.onchange = () => {
+    figura.style.backgroundColor = color.value;
 }
-radius.oninput=()=>{
-    figura.style.borderRadius=radius.value+"%";
-    document.getElementById("vradio").innerHTML=radius.value;
+radius.oninput = () => {
+    figura.style.borderRadius = radius.value + "%";
+    document.getElementById("vradio").innerHTML = radius.value;
 }
-anchoborde.oninput=()=>{
-    figura.style.borderWidth=anchoborde.value+"px";
-    document.getElementById("vanchoborde").innerHTML=anchoborde.value;
+anchoborde.oninput = () => {
+    figura.style.borderWidth = anchoborde.value + "px";
+    document.getElementById("vanchoborde").innerHTML = anchoborde.value;
 }
-tipoborde.onchange=()=>{
-    figura.style.borderStyle=tipoborde.value;
+tipoborde.onchange = () => {
+    figura.style.borderStyle = tipoborde.value;
 }
-colorborde.onchange=()=>{
-    figura.style.borderColor=colorborde.value;
+colorborde.onchange = () => {
+    figura.style.borderColor = colorborde.value;
+}
+
+function actualizarSombra() {
+    let x = ejeX.value;
+    let y = ejeY.value;
+    let b = blur.value;
+    let s = spread.value;
+    let colorS = colorsombra.value;
+    figura.style.boxShadow = `${x}px ${y}px ${b}px ${s}px ${colorS}`;
+}
+
+blur.oninput = () => {
+    document.getElementById("vblur").innerHTML = blur.value;
+    actualizarSombra();
+}
+spread.oninput = () => {
+    document.getElementById("vspread").innerHTML = spread.value;
+    actualizarSombra();
 }
